013c70ef6d9f9e8741213e08bebd6c47dee4f53b
Igor Loshachenko <igorl1201@gmaikl.com>
2025-04-14 14:37:13 +0300

    master: add logout

--- src/main/java/ru/lid/progertrainer/security/AuthenticationController.java
+++ src/main/java/ru/lid/progertrainer/security/AuthenticationController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,7 +66,7 @@ public class AuthenticationController {
         service.refreshToken(request, response);
     }
 
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public ResponseEntity<String> logout(
             HttpServletRequest request,
             HttpServletResponse response
